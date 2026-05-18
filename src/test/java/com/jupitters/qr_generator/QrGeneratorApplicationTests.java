1aea3f87b523d21b21bc6efaa1f88e3fb441f56d
jupitters <83190205+jupitters@users.noreply.github.com>
2026-05-17 22:50:31 -0300

    Add files via upload

---
+++ src/test/java/com/jupitters/qr_generator/QrGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupitters.qr_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
