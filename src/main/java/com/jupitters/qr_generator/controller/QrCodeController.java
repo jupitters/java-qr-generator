38c7fb242d966419d4993b075f4c0f66c0df44f5
jupitters <sampaiobale@gmail.com>
2026-05-22 11:30:40 -0300

    qr code controller

--- src/main/java/com/jupitters/qr_generator/controller/QrCodeController.java
+++ src/main/java/com/jupitters/qr_generator/controller/QrCodeController.java
@@ -2,6 +2,8 @@ package com.jupitters.qr_generator.controller;
 
 import com.jupitters.qr_generator.dto.QrCodeGenerateRequest;
 import com.jupitters.qr_generator.dto.QrCodeGenerateResponse;
+import com.jupitters.qr_generator.ports.StoragePort;
+import com.jupitters.qr_generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/qrcode")
 public class QrCodeController {
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+        try{
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
